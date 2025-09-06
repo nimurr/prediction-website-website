815fbe0b3aecd62b467461209832c0d5e602b2e8
nimurr <nimurnerob404@gmail.com>
2025-09-06 20:59:25 +0600

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export", // Static export
-    experimental: {
-        appDir: true,          // App Router enable
-        globalNotFound: true,  // Catch-all 404
-    },
-    reactStrictMode: true,
+    // output: "export", // Static export
+    // experimental: {
+    //     appDir: true,          // App Router enable
+    //     globalNotFound: true,  // Catch-all 404
+    // },
+    // reactStrictMode: true,
 };
 
 export default nextConfig;
